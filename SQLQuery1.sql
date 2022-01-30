5db122c4ffaf813746ff9fc7d83823969e6da81e
VT <55559682+Victor-T2001@users.noreply.github.com>
2022-01-31 00:52:39 +0200

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,22 +4,22 @@
 --drop table tb_Client_Status;
 
 create table tb_Treaty_Systems(
-	id		integer		not null,
-	Name	varchar(50) not null,
+	id	integer		not null,
+	Name	varchar(50)	not null,
 	primary key (id)
 );
 
 create table tb_Client_Status(
-	id		integer		not null,
-	Name	varchar(50) not null,
+	id	integer		not null,
+	Name	varchar(50) 	not null,
 	primary key (id)
 );
 
 create table tb_Clients(
-	id			integer		not null,
-	fname		varchar(50) not null,
-	mname		varchar(50) not null,
-	lname		varchar(50) not null,
+	id		integer		not null,
+	fname		varchar(50) 	not null,
+	mname		varchar(50) 	not null,
+	lname		varchar(50) 	not null,
 	RiskLevel	smallint	not null,
 	Date_from	date		not null,
 	Date_to		date		not null,
@@ -31,14 +31,14 @@ create table tb_Clients(
 );
 
 create table tb_Treaty(
-	id			integer			not null,
+	id		integer			not null,
 	ClientId	integer			not null,
 	SystemId	integer			not null,
-	Rate		numeric(18,2)	not null,
-	Closed		bit				not null,
-	Amount		numeric(18,2)	not null,
-	CCY			char(3)			null,
-	ExRate		numeric(18,2)	null,
+	Rate		numeric(18,2)		not null,
+	Closed		bit			not null,
+	Amount		numeric(18,2)		not null,
+	CCY		char(3)			null,
+	ExRate		numeric(18,2)		null,
 	primary key (id),
 	constraint FK_tb_Clients foreign key (ClientId)
 	references tb_Clients(id),
